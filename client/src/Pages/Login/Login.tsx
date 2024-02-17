3178d8d11888a096df12fc7dfbd0bad66b458f3e
shay3520 <shayliebling3520@gmail.com>
2024-02-17 13:11:56 +0200

    start of Login page

---
+++ client/src/Pages/Login/Login.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, TextField, Box, Typography, Container } from '@mui/material';
+
+export const Login = () => {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Login Page
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
